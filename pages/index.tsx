7248d4c06986db0958e1d4ac4dbe5b96bea7b72f
Martin Utama <martin.bcu@gmail.com>
2022-07-16 14:54:38 +0900

    table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
-import { Breadcrumbs, Anchor } from '@mantine/core';
 import type { ReactElement } from 'react'
 import Layout from '../components/layout'
 import { NextPageWithLayout } from './_app';
 import useUser from '../lib/useUser'
 import { useRouter } from 'next/router'
 import fetchJson from '../lib/fetchJson'
-
+import { Table, Breadcrumbs, Anchor, AppShell, Navbar, Header, Card, Paper } from '@mantine/core';
 
 const Home: NextPageWithLayout = () => {
 
@@ -28,62 +26,76 @@ const Home: NextPageWithLayout = () => {
     </Anchor>
   ));
 
+
+  const tableRows = [
+    { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+    { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+    { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+    { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+    { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+  ].map((element) => (
+    <tr key={element.name}>
+      <td>{element.position}</td>
+      <td>{element.name}</td>
+      <td>{element.symbol}</td>
+      <td>{element.mass}</td>
+    </tr>
+  ));
+
   return (
-    <main className={styles.main}>
-      <Breadcrumbs separator="→">{items}</Breadcrumbs>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
 
-      <p className={styles.description}>
-        Get started by editing{' '}
-        <code className={styles.code}>pages/index.tsx</code>
-      </p>
+    <AppShell
+      padding="md"
+      header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      <main className={styles.main}>
+        <Breadcrumbs separator="→">{items}</Breadcrumbs>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
 
-      <a
-        href="/api/logout"
-        onClick={async (e) => {
-          e.preventDefault()
-          mutateUser(
-            await fetchJson('/api/logout', { method: 'POST' }),
-            false
-          )
-          router.push('/login')
-        }}
-      >
-        Logout
-      </a>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
 
         <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
+          href="/api/logout"
+          onClick={async (e) => {
+            e.preventDefault()
+            mutateUser(
+              await fetchJson('/api/logout', { method: 'POST' }),
+              false
+            )
+            router.push('/login')
+          }}
         >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+          Logout
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+        <div className={styles.grid}>
+          <Paper
+            shadow="sm"
+            p="xl">
+            <Table>
+              <thead>
+                <tr>
+                  <th>Element position</th>
+                  <th>Element name</th>
+                  <th>Symbol</th>
+                  <th>Atomic mass</th>
+                </tr>
+              </thead>
+              <tbody>{tableRows}</tbody>
+            </Table>
+          </Paper>
+        </div>
+      </main>
+    </AppShell>
   )
 }
 
